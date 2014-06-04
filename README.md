63deb64a436580d3d0eaa946a49b0e7c4b118cb6
xmj <mingjie.xing@gmail.com>
2014-06-04 15:55:32 +0800

    Update README.md; add src, html, utils and build.sh.

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-100-gdb-tips
-============
+# 《100个gdb小技巧》
 
-A collect of gdb tips. 100 may just mean many here.
+一本关于gdb使用小技巧的手册。100，在这里可能只是表明很多；具体的数目取决于您的参与和贡献。
+
+## 联系方式
+
+- 博客网站：www.hellogcc.org
+- 在线讨论问题：IRC, freenode, #hellogcc房间
+- 邮件列表：http://www.freelists.org/list/hellogcc(发信需要先订阅)
+ 
+## 授权许可
+
+目前，本手册使用的是[GNU Free Documentation License](http://www.gnu.org/licenses/fdl.html)。
+
+## 致谢
+
+
+
+## 开始阅读
+[开始阅读](<https://github.com/hellogcc/100-gdb-tips/blob/master/ebook/index.md>)
+
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script invokes md2min to convert markdown files to minimal html files,
+# using github css. So go and md2min should be available before you run it. 
+# See https://github.com/fairlyblank/md2min
+# See http://code.google.com/p/go
+
+TOPDIR=`dirname $0`
+
+if [ ! type -P go >/dev/null 2>&1 ]; then
+  echo "error: can't find go, which is necessary for building html"
+  exit 0
+fi
+
+if [ ! type -P md2min >/dev/null 2>&1 ]; then
+  echo "error: can't find md2min, which is necessary for building html"
+  exit 0
+fi
+
+(
+export SRC="$TOPDIR/src"
+export HTML="$TOPDIR/html"
+go run "$TOPDIR/utils/build.go"
+)
+
